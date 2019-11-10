13f7d1f0b4196f4786d302beab9d551097c28f8c
Dragonaide <55767582+Dragonaide@users.noreply.github.com>
2019-11-10 11:47:46 -0500

    Update WorldManager.vb

--- Modules/WorldManager.vb
+++ Modules/WorldManager.vb
@@ -8,7 +8,7 @@ Module WorldManager
             System.IO.Path.Combine(root, "resource_packs")
             System.IO.Path.Combine(root, "behavior_packs")
             System.IO.Path.Combine(root, "levelname.txt")
-            System.IO.File.WriteAllLines(root + "/levelname.txt", "My World")
+            System.IO.File.WriteAllText(root + "/levelname.txt", "My World")
         End If
     End Function
 End Module
