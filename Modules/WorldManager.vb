774fcdedca13171cf9a315a0111202796aab0494
Dragonaide <55767582+Dragonaide@users.noreply.github.com>
2019-11-10 11:40:46 -0500

    Update WorldManager.vb

--- Modules/WorldManager.vb
+++ Modules/WorldManager.vb
@@ -2,6 +2,8 @@ Imports System
 Imports System.IO
 Module WorldManager
     Function MCPEWorld(root As String) As System.IO.Directory
-        
+        If Not System.IO.Directory.Exists(root) Then
+            System.IO.Directory.CreateDirectory(root)
+        End If
     End Function
 End Module
