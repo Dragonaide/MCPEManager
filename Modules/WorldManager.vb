909ad2f6d3700f83c53504754898431907cb7948
Dragonaide <55767582+Dragonaide@users.noreply.github.com>
2019-11-10 11:42:36 -0500

    Update WorldManager.vb

--- Modules/WorldManager.vb
+++ Modules/WorldManager.vb
@@ -4,6 +4,9 @@ Module WorldManager
     Function MCPEWorld(root As String) As System.IO.Directory
         If Not System.IO.Directory.Exists(root) Then
             System.IO.Directory.CreateDirectory(root)
+            System.IO.Path.Combine(root, "db")
+            System.IO.Path.Combine(root, "resource_packs")
+            System.IO.Path.Combine(root, "behavior_packs")
         End If
     End Function
 End Module
